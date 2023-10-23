af6efe4605b08d87ceb6aca0ed8bb957b0cc891f
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-10-23 10:05:01 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 1.Вывести на экран в столбик целые числа из интервала от 1 до n(для решения задачи использовать операторы циклов: for, while, do -while).
+            //Console.WriteLine("Введите n:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int i = 1;
+            //while (i <= n) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //    i++;
+            //}
+
+
+            //Задача 2.Вывести четные двузначные числа и подсчитать их количество.
+            //int a = 0;
+            //for (int i = 10; i <= 99; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        a++;
+            //        Console.WriteLine(i); 
+            //    }
+            //}
+            //Console.WriteLine($"Количество чётных двузначных чисел {a}");
+
+
+            //Задача 3.Составьте таблицу значений функции y = 2х2 + 4x - 5 на отрезке[-5; 5] с шагом h = 0,2
+            //for (double x = -5; x <= 5; x=x+0.2)
+            //{
+            //    Console.WriteLine($"x={x}\t y={2 * Math.Pow(x, 2) + 4 * x - 5}");
+            //}
+
+
+            // Задача 4.Найти сумму чисел, кратных 5, в пределах от 0 до 1000.
+            //int S = 0;
+            //for (int x = 0; x <= 1000; x++)
+            //{
+            //    if (x % 5 == 0)
+            //    {
+
+            //        S += x;
+            //    }
+            //}
+            //Console.WriteLine($"Сумма чисел, кратных 5 = {S}");
+
+
+            //Задача 5.Определить количество цифр заданного числа.
+            //Console.Write("Введите число: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            //while (number != 0)
+            //{
+            //    number /= 10;
+            //    count++;
+            //}
+            //Console.WriteLine($"Количество цифр заданного числа = {count}");
+  
+            Console.ReadKey();
+
+        }
+    }
+}
